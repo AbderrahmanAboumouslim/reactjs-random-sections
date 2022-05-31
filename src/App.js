249573d8b9652de4fa3ca25fca72ae16bee727fc
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-05-31 21:33:17 +0100

    using a chart library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import CircleChart from './components/circleChart/CircleChart';
+import { nftData } from './components/circleChart/data';
+import { styled } from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <CircleChart data={nftData} />;
+    </Wrapper>
   );
 }
+
+const Wrapepr = styled.div`
+  background-color: gray;
+`;
 
 export default App;
---
+++ src/components/circleChart/data.js
@@ -0,0 +1,27 @@
+// Preparing the chart data
+export const nftData = [
+  {
+    label: 'DeGods',
+    value: '550',
+  },
+  {
+    label: 'Trippin Ape Tribe',
+    value: '550',
+  },
+  {
+    label: 'Okay Bears',
+    value: '550',
+  },
+  {
+    label: 'KODS',
+    value: '550',
+  },
+  {
+    label: 'Blocksmith',
+    value: '555',
+  },
+  {
+    label: 'Rarikey',
+    value: '22',
+  },
+];
